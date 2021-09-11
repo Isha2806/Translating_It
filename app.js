8108ac4eb02a1cdd12e0613cc56e7aada44ca266
Isha <xishaapatel@gmail.com>
2021-09-11 18:40:15 +0530

    refact

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const input = document.querySelector('.wordInput');
 const translateButton = document.querySelector('.btn');
 const outputE = document.querySelector('#output');
+var serverUrl = "https://funtranslations.com/dolan";
+function getTranslation(text){
+    return serverUrl + "? "+ "text" + text
+}
+function errorHandler(){
+    console.log("error occured" , error);
 
-function translateText(){
-console.log('clicked');
 }
 
-translateButton.addEventListener('click',translateText)
+function translateText(){
+    var inputText = input.value;
+    fetch(getTranslation(inputText))
+    .then(response => response.json)
+    .then(json=> console.log(json.contents.translated))
+    .catch(errorHandler)
+
+  outputE.innerText = " In Dolan..... " + inputText;
+  
+  
+  
+};
+
+translateButton.addEventListener("click" ,translateText)
