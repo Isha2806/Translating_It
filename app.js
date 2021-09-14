fcf4ee92e95a4105854a08faa3810085b7bd5cd4
Isha <xishaapatel@gmail.com>
2021-09-14 19:58:00 +0530

    update css

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-const input = document.querySelector('.wordInput');
-const translateButton = document.querySelector('.btn');
-const outputE = document.querySelector('#output');
-var serverUrl = "https://funtranslations.com/dolan";
-function getTranslation(text){
-    return serverUrl + "? "+ "text" + text
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
 }
-function errorHandler(){
-    console.log("error occured" , error);
 
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
 }
 
-function translateText(){
-    var inputText = input.value;
-    fetch(getTranslation(inputText))
-    .then(response => response.json)
-    .then(json=> console.log(json.contents.translated))
-    .catch(errorHandler)
 
-  outputE.innerText = " In Dolan..... " + inputText;
-  
-  
-  
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
 };
 
-translateButton.addEventListener("click" ,translateText)
+btnTranslate.addEventListener("click", clickHandler)
